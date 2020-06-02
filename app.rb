1449a9bebef097f9476faca1e8e6f5958fe7d10c
proactive-analyst-6157 <u.batsaikhan@gmail.com>
2020-06-02 04:23:51 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,8 +2,16 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   
-  get '/' do
-    "Hello, World!"
+  get '/name' do
+    "My name is___"
+  end
+  
+  get '/hometown' do
+    "My hometown is___"
+  end
+  
+  get '/favorite-song' do
+    "My favorite song is___"
   end
   
 end
